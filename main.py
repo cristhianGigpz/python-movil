a33b35b21dfabe5a8069ecbae2a6582d623641b9
cristhianGigpz <cjacevedot@gmail.com>
2026-01-29 12:19:52 -0500

    video 2 encapsulamiento

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from personajes import Personaje
 
 goku = Personaje("Goku", 9001)
-vegeta = Personaje("Vegeta", 8500)
-bulma = Personaje("Bulma", 10)
+# vegeta = Personaje("Vegeta", 8500)
+# bulma = Personaje("Bulma", 10)
 
 print(goku.saludar())
-print(vegeta.saludar())
-print(bulma.saludar())
+#goku._Personaje__nivel_fuerza = 500
+goku.set_nivel_fuerza(500)
+print(goku.get_nivel_fuerza())
+# print(vegeta.saludar())
+# print(bulma.saludar())
--- personajes.py
+++ personajes.py
@@ -1,7 +1,16 @@
 class Personaje:
     def __init__(self, nombre, nivel_fuerza):
         self.nombre = nombre
-        self.nivel_fuerza = nivel_fuerza
+        self.__nivel_fuerza = nivel_fuerza
 
     def saludar(self):
-        return f"Hola, soy {self.nombre} y estoy en el nivel {self.nivel_fuerza}."
+        return f"Hola, soy {self.nombre}"
+    
+    def get_nivel_fuerza(self):
+        return self.__nivel_fuerza
+    
+    def set_nivel_fuerza(self, nuevo_nivel):
+        if nuevo_nivel > 0:
+            self.__nivel_fuerza = nuevo_nivel
+        else:
+            raise ValueError("El nivel de fuerza no puede ser negativo")
